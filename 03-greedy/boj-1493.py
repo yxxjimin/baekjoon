db4602f822fa3f8dd14a7e55be95527953879431
Jimin Yoo <jmy0819_@naver.com>
2024-01-17 20:11:36 +0900

    :sparkles: [BOJ] 1493

---
+++ 03-greedy/boj-1493.py
@@ -0,0 +1,33 @@
+# Read input
+length, width, height = map(int, input().split())
+target_volume = length * width * height
+
+n = int(input())
+cubes = []
+for _ in range(n):
+    length_exp, count = input().split()
+    cubes.append((int(length_exp), int(count)))
+cubes.sort(reverse=True)
+
+total_cubes = 0
+aggregated_volume =0
+for cube in cubes:
+    length_exp, cube_count = cube
+    cube_length = 2 ** length_exp
+    cube_volume = cube_length ** 3
+    
+    # Largest volume with current cube size
+    current_max_volume = (cube_volume * (length // cube_length) 
+                          * (width // cube_length) * (height // cube_length))
+
+    # Constructable volume with current cube count
+    constructed_volume = min(
+        current_max_volume - aggregated_volume, cube_volume * cube_count)
+    
+    aggregated_volume += constructed_volume
+    total_cubes += int(constructed_volume / cube_volume)
+
+if (aggregated_volume == target_volume):
+    print(total_cubes)
+else:
+    print(-1)
